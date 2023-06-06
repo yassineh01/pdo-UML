2e2ac97aa737e8746fcf72e1f0f8e3f4d763c61e
yassineh01 <132911589+yassineh01@users.noreply.github.com>
2023-06-06 11:04:27 +0200

    Add files via upload

---
+++ week2pdo.php
@@ -0,0 +1,24 @@
+<?php
+$host = 'localhost';
+$db   = 'winkels';
+$user = 'username';
+$pass = 'password';
+$charset = 'tre1l39';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "<p>Connected to the database successfully!</p>";
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
+
+
+?>
